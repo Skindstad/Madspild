f04b8df9da68d0a9d8db9b68f50ec067c8511108
Jakob Skindstad Frederiksen <jakobsfrederiksen1@gmail.com>
2024-01-05 15:21:08 +0100

    Database Tables and data

--- DataAndQuerys/OprettesAfDBOgTables.sql
+++ DataAndQuerys/OprettesAfDBOgTables.sql
@@ -1,2 +1,69 @@
+Drop Database FoodWaste;
 Create Database FoodWaste;
 
+Create Table AccessRight 
+(
+Id int not null Identity(1,1) Primary key,
+AccessName varchar(255) not null
+);
+
+
+Create Table Users 
+(
+Id int not null Identity(1,1) Primary key,
+Email varchar(255) Not Null,
+Pword varchar(255) Not null,
+AccessRight int not null FOREIGN KEY REFERENCES AccessRight(Id),
+);
+
+create table Zipcodes (
+  Code nchar(4) primary key,
+  City nvarchar(30) not null
+);
+
+Create Table Persons
+(
+Id int not null Primary Key FOREIGN KEY REFERENCES Users(Id),
+PersonName Varchar(255) not null,
+HomePhone nchar(10) not null,
+WorkPhone nchar(10) not null,
+Address varchar(255) not null,
+Zipcode nchar(4) not null FOREIGN KEY REFERENCES Zipcodes(Code)
+);
+
+Create Table Category 
+(
+Id int not null Identity(1,1) Primary key,
+CategoryName varchar(255) not null
+);
+
+Create Table Goods 
+(
+Id int not null Identity(1,1) Primary key,
+ProductName Varchar(255) not null,
+Price float not null,
+Amount int not null,
+AmountLimit int not null,
+Category int Foreign Key references Category(id),
+PicturePath varchar(255)
+);
+
+
+Create Table Orders
+(
+Id int not null Identity(1,1) Primary key,
+PersonId int not null FOREIGN KEY REFERENCES persons(Id),
+FullPrice float not null,
+BoughtDato nchar(10) not null
+)
+
+Create table PersonGoods 
+(
+Id int not null Identity(1,1) Primary key,
+PersonId int not null FOREIGN KEY REFERENCES persons(Id),
+ProductId int not null FOREIGN KEY REFERENCES Goods(Id),
+Amount int not null,
+BasketDato nchar(10) not null,
+Bought Varchar(5) DEFAULT  'false',
+BoughtDato int FOREIGN KEY REFERENCES Orders(Id)
+)
