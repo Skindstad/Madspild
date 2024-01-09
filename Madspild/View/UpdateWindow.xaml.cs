afc508eea5cf6a9f5e04d278d07161559ca75086
Yaruohri <bendixforsberg@hotmail.com>
2024-01-09 19:28:30 +0100

    Made the UpdateWindow

---
+++ Madspild/View/UpdateWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Madspild.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Madspild.View
+{
+    /// <summary>
+    /// Interaction logic for UpdateWindow.xaml
+    /// </summary>
+    public partial class UpdateWindow : Window
+    {
+        private AdminViewModel admin;
+        public UpdateWindow()
+        {
+            InitializeComponent();
+            admin.WarningHandler += delegate (object sender, MessageEventArgs e) {
+                MessageBox.Show(e.Message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            };
+            admin.CloseHandler += delegate (object sender, EventArgs e) { Close(); };
+            DataContext = admin;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the window
+            Close();
+        }
+    }
+}
